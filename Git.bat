b749aa95e759d2b6d6c09bc754b5d65cf1d6bebd
SergeyKitsul <tiger60510@gmail.com>
2023-09-10 01:41:59 +0300

    commit

--- Git.bat
+++
@@ -1,4 +0,0 @@
-set /P txtcommit="Введите текст комментария: "
-git add .
-git commit -m "%date% %time%: %txtcommit%"
-git push
--- Git1111.bat
+++
@@ -1,4 +0,0 @@
-cd "C:\OSPanel\domains\GitHub\Kitsul"
-git add --all
-git commit -m "%date% %time% %txtcommit%"
-git push
